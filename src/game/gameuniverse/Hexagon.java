765e2a99aeadc9830017066c06edf76b24a7f497
Kim, Sae Hun <kim.saehun253@gmail.com>
2015-10-12 19:38:19 -0700

    Add Hexagon class.

---
+++ src/game/gameuniverse/Hexagon.java
@@ -0,0 +1,33 @@
+package game.gameuniverse;
+
+/**
+ * Provides a model for a hexagon, a plane figure with 6 sides.
+ *
+ * Hexagons will be
+ *     i) vertically Oriented (pointy top).
+ *     ii) a regular hexagon. All sides have equal length.
+ */
+public class Hexagon {
+    private double sideLength;
+
+    /**
+     * Constructor
+     */
+    public Hexagon() {
+        sideLength = 0;
+    }
+
+    /**
+     * Constructor
+     *
+     * @param sideLength the length of a side.
+     */
+    public Hexagon(double sideLength) {
+        this.sideLength = sideLength;
+    }
+
+    // Helper Functions
+    public double getSideLength() { return sideLength; }
+
+    public void setSideLength(double sideLength) { this.sideLength = sideLength; }
+}
