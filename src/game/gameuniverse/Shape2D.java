03456268125339f11091761b3157be99ae94695a
Kim, Sae Hun <kim.saehun253@gmail.com>
2015-10-12 19:46:57 -0700

    Add Shape2D class.

---
+++ src/game/gameuniverse/Shape2D.java
@@ -0,0 +1,10 @@
+package game.gameuniverse;
+
+/**
+ * Abstract class for a 2 dimensional shape
+ */
+public abstract class Shape2D {
+    public abstract void setSideLength(double sideLength);
+
+    public abstract double getSideLength();
+}
