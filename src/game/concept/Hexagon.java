f16d31a051abc3dbde2c26f9f131d6299ae51dfb
Kim, Sae Hun <kim.saehun253@gmail.com>
2015-10-17 00:34:53 -0700

    Add orientation and refactor code.

--- src/game/concept/Hexagon.java
+++ src/game/concept/Hexagon.java
@@ -2,18 +2,20 @@ package game.concept;
 
 /**
  * Provides a model for a hexagon, a plane figure with 6 sides.
- *
- * Hexagons will be
- *     i) vertically Oriented (pointy top).
- *     ii) a regular hexagon. All sides have equal length.
  */
 public class Hexagon extends Shape2D{
+    public enum Orientation {
+        VERTICAL, HORIZONTAL
+    }
+
+    private Orientation orientation;
     private double sideLength;
 
     /**
      * Default Constructor
      */
     public Hexagon() {
+        orientation = Orientation.VERTICAL;
         sideLength = 0;
     }
 
@@ -22,12 +24,27 @@ public class Hexagon extends Shape2D{
      *
      * @param sideLength the length of a side.
      */
-    public Hexagon(double sideLength) {
+    public Hexagon(Orientation orientation, double sideLength) {
+        this.orientation = orientation;
         this.sideLength = sideLength;
     }
 
     // Getters and Setters
-    public double getSideLength() { return sideLength; }
+    public Orientation getOrientation() {
+        return orientation;
+    }
 
-    public void setSideLength(double sideLength) { this.sideLength = sideLength; }
+    public void setOrientation(Orientation orientation) {
+        this.orientation = orientation;
+    }
+
+    @Override
+    public double getSideLength() {
+        return sideLength;
+    }
+
+    @Override
+    public void setSideLength(double sideLength) {
+        this.sideLength = sideLength;
+    }
 }
